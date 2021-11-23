60bf8a5030eb7bf6e82608474f2952ed4a89a28a
altynai2002 <altynay.bakytbekova.02@mail.ru>
2021-11-24 00:29:19 +0600

    hw

--- app/src/main/java/com/pro/programayimapp/MainActivity4.kt
+++ app/src/main/java/com/pro/programayimapp/MainActivity4.kt
@@ -1,11 +1,38 @@
 package com.pro.programayimapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val layoutManager = LinearLayoutManager(this)
+        val adapter = SimpleAdapter {
+            Toast.makeText(this, "ITEM -$it", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("text", "ITEM -$it")
+            startActivity(intent)
+        }
+
+
+        recycler.layoutManager = layoutManager
+        recycler.adapter = adapter
+        recycler.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
+
+        val list = mutableListOf<String>()
+        for (i in 0..20) {
+            list.add("ITEM -$i")
+
+        }
+        adapter.setData(list)
+
     }
 }
