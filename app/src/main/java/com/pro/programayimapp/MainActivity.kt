4a77c6d926e63ce626343620a05a312de610ec2d
altynai2002 <altynay.bakytbekova.02@mail.ru>
2021-11-28 22:31:13 +0600

    homework11_email_sender

--- app/src/main/java/com/pro/programayimapp/MainActivity.kt
+++ app/src/main/java/com/pro/programayimapp/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.pro.programayimapp
 
-import android.graphics.Color
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.AppCompatButton
-import androidx.constraintlayout.widget.ConstraintLayout
+import com.pro.programayimapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendBtn.setOnClickListener {
+            val email = binding.email.text.toString()
+            val subject = binding.editSubject.text.toString()
+            val letter = binding.editLetter.text.toString()
+
+            val emails = email.split(",".toRegex()).toTypedArray()
+
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, emails)
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, letter)
+            }
+
+            if (intent.resolveActivity(packageManager) != null){
+                startActivity(intent)
+            }else{
+                Toast.makeText(this@MainActivity, "Приложение не установлено", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
