f93e309c016986cb20b43cb3478afc07562a1109
altynai2002 <altynay.bakytbekova.02@mail.ru>
2021-12-02 00:00:12 +0600

    homework9

--- app/src/main/java/com/pro/programayimapp/MainActivity.kt
+++ app/src/main/java/com/pro/programayimapp/MainActivity.kt
@@ -9,8 +9,37 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
+    private lateinit var txt: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txt = findViewById(R.id.txt)
+
+        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
+
+        btn1.setOnClickListener{
+            ++count
+
+            txt.text = count.toString()
+        }
+
+        btn2.setOnClickListener {
+            count = 0
+            txt.text = count.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("key", count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt("key")
+        txt.text = count.toString()
     }
 }
