7cbf9878b2bc7eef9cab193f3afe24c49d61f46c
altynai2002 <altynay.bakytbekova.02@mail.ru>
2021-12-01 23:47:56 +0600

    homework8

--- app/src/main/java/com/pro/programayimapp/MainActivity2.kt
+++ app/src/main/java/com/pro/programayimapp/MainActivity2.kt
@@ -13,5 +13,33 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trafficlight)
+
+        linLayout = findViewById(R.id.layout)
+        txtView = findViewById(R.id.txtView)
+
+        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
+        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
+
+        btn1.setOnClickListener {
+            txtView.text = "RED"
+            txtView.setTextColor(Color.RED)
+            linLayout.setBackgroundColor(Color.RED)
+            btn1.setTextColor(Color.RED)
+        }
+
+        btn2.setOnClickListener {
+            txtView.text = "YELLOW"
+            txtView.setTextColor(Color.YELLOW)
+            linLayout.setBackgroundColor(Color.YELLOW)
+            btn2.setTextColor(Color.YELLOW)
+        }
+
+        btn3.setOnClickListener {
+            txtView.text = "GREEN"
+            txtView.setTextColor(Color.GREEN)
+            linLayout.setBackgroundColor(Color.GREEN)
+            btn3.setTextColor(Color.GREEN)
+        }
     }
 }
