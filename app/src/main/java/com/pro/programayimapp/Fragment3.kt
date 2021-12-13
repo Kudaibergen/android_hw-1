ed5e4209c34208d9cf1541ab505cc8618259345f
Kudaybergen <tuleubekov.kas@gmail.com>
2021-12-13 06:22:31 +0600

    comment: hw-18

--- app/src/main/java/com/pro/programayimapp/Fragment3.kt
+++ app/src/main/java/com/pro/programayimapp/Fragment3.kt
@@ -11,8 +11,8 @@ class Fragment3: Fragment(R.layout.fragment_3) {
         super.onViewCreated(view, savedInstanceState)
         val toolbar = view.findViewById<Toolbar>(R.id.fr3_toolbar)
         toolbar.setNavigationOnClickListener {
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, Fragment2())
+            requireActivity().supportFragmentManager.beginTransaction() // hw-18: здесь не правильно. Получается мы добавляем фрагмент в стек
+                .replace(R.id.fragment_container, Fragment2()) // нам же нужно вернуться назад. Подсказка: onBackPressed()
                 .disallowAddToBackStack()
                 .commit()
         }
